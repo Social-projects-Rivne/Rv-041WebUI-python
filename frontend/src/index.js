13c82bcbefd6bcbe19d24a3e2aab06169a5b0045
viktormovchaniuk <vityamovchanyk@gmail.com>
2018-12-28 12:02:12 +0200

    add material-ui library and temporary remove redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Router from './components/router/router';
-import { createStore } from 'redux';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-
-import { rootReducer } from './reducers/redusers';
-
-const store = createStore(rootReducer);
+import React from "react";
+import ReactDOM from "react-dom";
+import Router from "./components/router/router";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-    <Provider store={store}>
-        <BrowserRouter>
-            <Router />
-        </BrowserRouter>
-    </Provider>
-    , document.getElementById('root'));
+  <BrowserRouter>
+    <Router />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
