c0d8d5bd6857a66bd4d71763360762470dcea2d4
Max <m.sukhovetskyi@gmail.com>
2019-01-03 21:37:59 +0200

    fix no such function error in tag_controler

--- easyrest/views/tag_controler.py
+++ easyrest/views/tag_controler.py
@@ -39,7 +39,7 @@ def get_tags_controler(request):
     query = request.dbsession.query(Tag)
     tags = query.all()
     if not tags:
-        body = warp([], False, "No tags in database")
+        body = wrap([], False, "No tags in database")
     else:
         tags_as_dict = [tag.as_dict() for tag in tags]
         for tag in tags_as_dict:
