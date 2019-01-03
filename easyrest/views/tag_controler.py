019275faa7b262ae272886498f16859a92aaa012
Max <m.sukhovetskyi@gmail.com>
2019-01-03 20:50:47 +0200

    Added docstring for tag_controller

--- easyrest/views/tag_controler.py
+++ easyrest/views/tag_controler.py
@@ -13,6 +13,31 @@ from ..models.Tag import Tag
 
 @view_config(route_name='get_tags', renderer='json', request_method='GET')
 def get_tags_controler(request):
+    """GET request controler to return tags
+    Args:
+        request: current pyramid request
+    Returns:
+        Json string(not pretty) created from dictionary with format:
+            {
+                "data": data,
+                "success": success,
+                "error": error
+            }
+        Where data is list with tags.
+        Style:
+            [
+                {
+                    "id": "tagId" + id
+                    "name": (str)
+                    "priority": (int)
+                }, ]
+        If tags not found then returns:
+        {
+            "data": [],
+            "success": False,
+            "error": No tags in database
+        }
+    """
     query = request.dbsession.query(Tag)
     tags = query.all()
     if len(tags) == 0:
