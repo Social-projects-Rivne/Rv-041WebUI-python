3a0c27f9c6a3723e08049804221509059523d868
Max <m.sukhovetskyi@gmail.com>
2019-01-03 20:47:31 +0200

    Refactor tag_controler, make simpler

--- easyrest/views/tag_controler.py
+++ easyrest/views/tag_controler.py
@@ -6,26 +6,21 @@ This module describes behavior of /tag route
 from pyramid.view import view_config
 from pyramid.response import Response
 
-from pyramid.httpexceptions import HTTPNotFound
-
-from sqlalchemy.exc import DBAPIError
-
-from json import dumps
+from ..scripts.json_helpers import wrap
 
 from ..models.Tag import Tag
 
 
 @view_config(route_name='get_tags', renderer='json', request_method='GET')
 def get_tags_controler(request):
-    try:
-        query = request.dbsession.query(Tag)
-        tags = query.all()
-        if len(tags) == 0:
-            raise HTTPNotFound()
-        dict_objs = [tag.as_dict() for tag in tags]
-        for tag in dict_objs:
+    query = request.dbsession.query(Tag)
+    tags = query.all()
+    if len(tags) == 0:
+        body = warp([], False, "No tags in database")
+    else:
+        tags_as_dict = [tag.as_dict() for tag in tags]
+        for tag in tags_as_dict:
             tag["id"] = "tagId" + str(tag["id"])
-        response = Response(body=dumps({"data": dict_objs, "name": "tag"}))
-    except DBAPIError as db_err_msg:
-        return Response(db_err_msg, content_type='text/plain', status=500)
+        body = wrap(tags_as_dict)
+    response = Response(body=body)
     return response
