f1f3770b942b1168f19602655d50682624fd5c28
Max <m.sukhovetskyi@gmail.com>
2019-01-03 19:13:45 +0200

    Add wrap function for making json response

---
+++ easyrest/scripts/json_helpers.py
@@ -0,0 +1,28 @@
+"""
+Script contains helper functions to work with json such as:
+    wrap(): wraper to make response json acording to format
+"""
+from json import dumps
+
+
+def wrap(data, error=None, success=True):
+    """Function to turn python dictionary from models output to
+    json format for futher send with responce
+    Args:
+        data (list): list of dictionaries representing models output
+        error (str): error message, None by default
+        success (bool): request status True is request succeeded
+            otherwise False
+    Returns:
+        Json string(not pretty) created from dictionary with format:
+            {
+                "data": data,
+                "success": success,
+                "error": error
+            }"""
+    data_dict = {
+        "data": data,
+        "success": success,
+        "error": error
+    }
+    return dumps(data_dict)
