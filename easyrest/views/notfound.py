b197e7a5f22fdbb2743d511b56fd93cab6663da8
Max <m.sukhovetskyi@gmail.com>
2019-01-04 16:44:44 +0200

    Json 404 page

--- easyrest/views/notfound.py
+++ easyrest/views/notfound.py
@@ -4,8 +4,9 @@ cookiecutter
 
 from pyramid.view import notfound_view_config
 
+from ..scripts.json_helpers import wrap
 
-@notfound_view_config(renderer='../templates/404.jinja2')
-def notfound_view(request):
-    request.response.status = 404
-    return {}
+
+@notfound_view_config(renderer='json')
+def notfound_view(error, request):
+    return wrap([], False, "%s: %s" % (error.title, error.args[0]))
