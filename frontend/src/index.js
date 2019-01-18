8e4a3f9c53867cc978ed523502034f3f085d73d8
viktormovchaniuk <vityamovchanyk@gmail.com>
2019-01-18 10:54:15 +0200

    commit changes before eject

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,4 +4,21 @@ import "./index.css";
 
 import App from "./containers/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { AppContainer } from "react-hot-loader";
+
+const render = () => {
+  ReactDOM.render(
+    <AppContainer>
+      <App />
+    </AppContainer>,
+    document.getElementById("root")
+  );
+};
+
+render();
+
+if (module.hot) {
+  module.hot.accept("./containers/App", () => {
+    render();
+  });
+}
