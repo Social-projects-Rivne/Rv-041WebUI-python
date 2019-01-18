9f6c0e80d8e0a1fb51df92a96b73be36e276c3f2
class <mailmax67@gmail.com>
2019-01-18 15:22:05 +0200

    Add Snackbar error message

--- easyrest/views/forbiden.py
+++ easyrest/views/forbiden.py
@@ -10,4 +10,4 @@ from ..scripts.json_helpers import wrap
 
 @forbidden_view_config(renderer='json')
 def notfound_view(error, request):
-    return wrap([], False, "%s: %s" % (error.title, error.args[0]))
+    return wrap([], False, "%s" % (error.args[0]))
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -9,6 +9,8 @@ import {
   Typography
 } from "@material-ui/core/";
 import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
+import Snackbar from "@material-ui/core/Snackbar";
+import SnackbarContent from "./SnackbarContent";
 
 const styles = theme => ({
   root: {
@@ -29,6 +31,13 @@ class Login extends React.Component {
     };
   }
 
+  handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    this.setState({ error: false });
+  };
+
   handleChange = (event, value) => {
     this.setState({ [value]: event.target.value });
   };
@@ -39,50 +48,38 @@ class Login extends React.Component {
       email: this.state.email,
       password: this.state.password
     };
-    if (body.email === "") {
-      this.setState({
-        error: true,
-        errorMes: "Email field cannot be empty"
-      });
-    } else if (body.password === "") {
-      this.setState({
-        error: true,
-        errorMes: "Password field cannot be empty"
-      });
-    } else {
-      fetch("http://localhost:6543/api/login", {
-        method: "POST",
-        body: JSON.stringify(body),
-        headers: { "Content-Type": "application/json" }
-      })
-        .then(response => response.json())
-        .then(input => {
-          const { success, error, data } = input;
-          const { role, token } = data;
-          if (success && role && token) {
-            localStorage.setItem("token", token);
-            localStorage.setItem("role", role);
-            this.props.state.changeState({
-              auth: true,
-              token,
-              role
-            });
-            this.setState({
-              error: false,
-              errorMes: ""
-            });
-            this.props.history.push("/restaurants");
-          } else {
-            throw error;
-          }
-        })
-        .catch(error => {
-          this.setState({
-            error: true,
-            errorMes: error
+    fetch("http://localhost:6543/api/login", {
+      method: "POST",
+      body: JSON.stringify(body),
+      headers: { "Content-Type": "application/json" }
+    })
+      .then(response => response.json())
+      .then(input => {
+        const { success, error, data } = input;
+        const { role, token } = data;
+        if (success && role && token) {
+          localStorage.setItem("token", token);
+          localStorage.setItem("role", role);
+          this.props.state.changeState({
+            auth: true,
+            token,
+            role
           });
+          this.setState({
+            error: false,
+            errorMes: ""
+          });
+          this.props.history.push("/restaurants");
+        } else {
+          throw error;
+        }
+      })
+      .catch(error => {
+        this.setState({
+          error: true,
+          errorMes: error
         });
-    }
+      });
   };
 
   render() {
@@ -91,65 +88,72 @@ class Login extends React.Component {
     return (
       <div className={classes.root}>
         <Card>
-          <form onSubmit={this.handleSubmit}>
-            <CardHeader
-              title={
-                <Typography variant="h5" align="center">
-                  Log In to Your Account
-                </Typography>
-              }
-            />
-            <CardContent>
-              <ValidatorForm
-                ref="form"
-                onSubmit={this.handleSubmit}
-                debounceTime={500}
-              >
-                <Grid container spacing={24}>
-                  <Grid item xs={12}>
-                    <TextValidator
-                      label="Enter your Email"
-                      onChange={e => this.handleChange(e, "email")}
-                      validators={["required", "isEmail"]}
-                      errorMessages={[
-                        "Email is required",
-                        "Email is not valid"
-                      ]}
-                      value={this.state.email}
-                      name="email"
-                      fullWidth
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <TextValidator
-                      label="Enter your Password"
-                      onChange={e => this.handleChange(e, "password")}
-                      validators={["required"]}
-                      errorMessages={["Password is required"]}
-                      value={this.state.password}
-                      name="password"
-                      type="password"
-                      fullWidth
-                    />
-                  </Grid>
-                  {error && (
-                    <Grid item xs={12}>
-                      <Typography variant="h4" align="center">
-                        {errorMes}
+          <CardHeader
+            title={
+              <Typography variant="h5" align="center">
+                Log In to Your Account
+              </Typography>
+            }
+          />
+          <CardContent>
+            <ValidatorForm
+              ref="form"
+              onSubmit={e => this.handleSubmit(e)}
+              debounceTime={500}
+            >
+              <Grid container spacing={24}>
+                <Grid item xs={12}>
+                  <TextValidator
+                    label="Enter your Email"
+                    onChange={e => this.handleChange(e, "email")}
+                    validators={["required", "isEmail"]}
+                    errorMessages={["Email is required", "Email is not valid"]}
+                    value={this.state.email}
+                    name="email"
+                    fullWidth
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextValidator
+                    label="Enter your Password"
+                    onChange={e => this.handleChange(e, "password")}
+                    validators={["required"]}
+                    errorMessages={["Password is required"]}
+                    value={this.state.password}
+                    name="password"
+                    type="password"
+                    fullWidth
+                  />
+                </Grid>
+                <Snackbar
+                  anchorOrigin={{
+                    vertical: "bottom",
+                    horizontal: "right"
+                  }}
+                  open={this.state.error}
+                  autoHideDuration={10000}
+                  onClose={this.handleClose}
+                >
+                  <SnackbarContent
+                    onClose={this.handleClose}
+                    variant="error"
+                    message={
+                      <Typography color="inherit" align="center">
+                        {errorMes ? errorMes : "No connection to the server"}
                       </Typography>
-                    </Grid>
-                  )}
-                  <Grid item xs={12}>
-                    <Grid container justify="flex-end">
-                      <Button type="submit" color="primary" variant="contained">
-                        Submit
-                      </Button>
-                    </Grid>
+                    }
+                  />
+                </Snackbar>
+                <Grid item xs={12}>
+                  <Grid container justify="flex-end">
+                    <Button type="submit" color="primary" variant="contained">
+                      Submit
+                    </Button>
                   </Grid>
                 </Grid>
-              </ValidatorForm>
-            </CardContent>
-          </form>
+              </Grid>
+            </ValidatorForm>
+          </CardContent>
         </Card>
       </div>
     );
