0a04fe85dd9bd5c9990c7551dd9a0c6fb1850684
Ivan A <riquadro2012@gmail.com>
2019-01-24 17:20:25 +0200

    add docstrings into validator module

--- easyrest/models/validator.py
+++ easyrest/models/validator.py
@@ -1,8 +1,22 @@
+"""This module describe data validation"""
+
 import jsonschema
 from jsonschema.validators import Draft4Validator
 
 
 def validation(schema, data):
+    """This function validates the data.
+
+    The function gets the schema template and data for validation.
+
+    :param schema: Schema for validation. See example https://goo.gl/BCH8r6
+    :type schema: dict
+    :param data: Data to validate
+    :type data: dict
+    :return: True if validation is ok
+    :rtype: bool
+    :raise: Validation error if validation with errors
+    """
     rules = Draft4Validator(schema, format_checker=jsonschema.FormatChecker())
     errors = sorted(rules.iter_errors(data), key=lambda e: e.path)
     if errors:
