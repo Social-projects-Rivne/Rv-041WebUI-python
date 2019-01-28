3b760f580c96c12a866d5c7e5114e9df2362eaf9
viktormovchaniuk <vityamovchanyk@gmail.com>
2019-01-28 12:41:22 +0200

    initial

---
+++ frontend/src/components/UserRestaurants/AddRestaurantForm.jsx
@@ -0,0 +1,177 @@
+import React from "react";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
+import AddIcon from "@material-ui/icons/Add";
+import Edit from "@material-ui/icons/Edit";
+import { green, amber } from "@material-ui/core/colors/";
+import {
+  Fab,
+  withStyles,
+  Typography,
+  Collapse,
+  Card,
+  CardContent,
+  TextField,
+  Grid,
+  Button,
+  Select,
+  ListItemText,
+  MenuItem,
+  Input,
+  Checkbox,
+  InputLabel,
+  FormControl,
+  Snackbar,
+  Tooltip,
+  CardHeader,
+  Divider
+} from "@material-ui/core";
+import TagSelect from "./TagSelect";
+
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: 250
+    }
+  }
+};
+
+export class AddRestaurantForm extends React.Component {
+  state = {
+    newRestaurant: {
+      name: "",
+      address: "",
+      phone: "",
+      description: "",
+      tags: []
+    },
+    allTags: []
+  };
+
+  componentDidMount() {
+    fetch("http://localhost:6543/api/tag")
+      .then(response => response.json())
+      .then(tags => this.setState({ allTags: tags.data }));
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const { newRestaurant } = this.state;
+
+    fetch("http://localhost:6543/api/user_restaurants", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "x-auth-token": localStorage.getItem("token")
+      },
+      body: JSON.stringify(newRestaurant)
+    }).then(response => {
+      return response.status >= 200 && response.status < 300
+        ? response.json()
+        : response.json().then(Promise.reject.bind(Promise));
+    });
+  };
+
+  handleFormChange = event => {
+    let name = event.target.name;
+    let value = event.target.value;
+
+    this.setState(prevState => ({
+      newRestaurant: {
+        ...prevState.newRestaurant,
+        [name]: value
+      }
+    }));
+  };
+
+  handleTagsChange = event => {
+    let value = event.target.value;
+    let name = event.target.name;
+
+    this.setState(prevState => ({
+      newRestaurant: { ...prevState.newRestaurant, [name]: value }
+    }));
+  };
+
+  render() {
+    const { allTags, newRestaurant } = this.state;
+
+    return (
+      <form
+        onSubmit={this.handleSubmit}
+        onChange={this.handleFormChange}
+        noValidate
+        autoComplete="off"
+      >
+        <Grid container spacing={16} justify="space-between">
+          <Grid item xs={12}>
+            <TextField
+              value={newRestaurant.name}
+              required
+              name="name"
+              label="Restaurant Name"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              value={newRestaurant.address}
+              required
+              name="address"
+              label="Restaurant Address"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              value={newRestaurant.phone}
+              name="phone"
+              label="Restaurant Phone"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              value={newRestaurant.description}
+              name="description"
+              label="Restaurant Description"
+              multiline
+              rows="4"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TagSelect
+              tags={newRestaurant.tags}
+              allTags={allTags}
+              onTagsChange={this.handleTagsChange}
+            />
+          </Grid>
+          <Grid item xs={3}>
+            <Button
+              onClick={this.handleCloseFormClick}
+              variant="contained"
+              color="secondary"
+              fullWidth
+            >
+              Cancel
+            </Button>
+          </Grid>
+          <Grid item xs={3}>
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              fullWidth
+              onClick={this.handleFormSubmit}
+            >
+              Add
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+    );
+  }
+}
+
+export default AddRestaurantForm;
--- frontend/src/components/UserRestaurants/AddUpdateRestaurant.jsx
+++ frontend/src/components/UserRestaurants/AddUpdateRestaurant.jsx
@@ -1,9 +1,5 @@
 import React from "react";
-import IconButton from "@material-ui/core/IconButton";
-import CloseIcon from "@material-ui/icons/Close";
 import AddIcon from "@material-ui/icons/Add";
-import Edit from "@material-ui/icons/Edit";
-import { green, amber } from "@material-ui/core/colors/";
 import {
   Fab,
   withStyles,
@@ -11,26 +7,20 @@ import {
   Collapse,
   Card,
   CardContent,
-  TextField,
   Grid,
   Button,
-  Select,
-  ListItemText,
-  MenuItem,
-  Input,
-  Checkbox,
-  InputLabel,
-  FormControl,
-  Snackbar
+  Tooltip,
+  CardHeader,
+  Divider
 } from "@material-ui/core";
 import classnames from "classnames";
+import AddRestaurantForm from "./AddRestaurantForm";
 
 const styles = theme => ({
-  header: {
+  root: {
     display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    marginBottom: theme.spacing.unit * 2
+    justifyContent: "flex-end",
+    padding: `${theme.spacing.unit * 2}px 0`
   },
   fab: {
     transform: "scale(1)",
@@ -38,67 +28,17 @@ const styles = theme => ({
   },
   fabDisabled: {
     transform: "scale(0)"
-  },
-  btns: {
-    marginTop: theme.spacing.unit * 2
-  },
-  listItem: {
-    textTransform: "capitalize"
-  },
-  snackbarSuccess: {
-    backgroundColor: green[600]
-  },
-  snackbarError: {
-    backgroundColor: amber[700]
   }
 });
 
-const MenuProps = {
-  PaperProps: {
-    style: {
-      maxHeight: 250
-    }
-  }
-};
-
 class AddUpdateRestaurant extends React.Component {
   state = {
-    name: "",
-    address: "",
-    phone: "",
-    description: "",
-    tags: [],
-    allTags: [],
-    formExpanded: false,
-    snackbarOpen: false,
-    isValid: true,
-    success: false,
-    snackbarMsg: "Error"
+    formExpanded: false
   };
-
-  componentDidMount() {
-    this.props.requestType === "put" &&
-      fetch(`http://localhost:6543/api/restaurant/${this.props.id}`)
-        .then(response => response.json())
-        .then(rest => rest.data[0])
-        .then(restInfo =>
-          this.setState({
-            name: restInfo.name,
-            address: restInfo.address_id,
-            phone: restInfo.phone,
-            description: restInfo.description,
-            tags: [...restInfo.tags]
-          })
-        );
-
-    fetch("http://localhost:6543/api/tag")
-      .then(response => response.json())
-      .then(tags => this.setState({ allTags: tags.data }));
-  }
 
   handleExpandFormClick = () => {
     this.setState({
-      formExpanded: !this.state.formExpanded
+      formExpanded: true
     });
   };
 
@@ -108,280 +48,44 @@ class AddUpdateRestaurant extends React.Component {
     });
   };
 
-  handleSubmit = (event, requestType, restId) => {
-    event.preventDefault();
-    const { name, address, description, phone, tags } = this.state;
-
-    const newRest = { name, address, description, phone, tags };
-
-    if (requestType === "post" && (!name.trim() || !address.trim())) {
-      this.setState({
-        isValid: false,
-        snackbarOpen: true,
-        snackbarMsg: "Fill all required fields",
-        success: false
-      });
-      return;
-    }
-
-    switch (requestType) {
-      // eslint-disable-next-line
-      case "post":
-        return fetch("http://localhost:6543/api/user_restaurants", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            "x-auth-token": localStorage.getItem("token")
-          },
-          body: JSON.stringify(newRest)
-        })
-          .then(response => {
-            return response.status >= 200 && response.status < 300
-              ? response.json()
-              : response.json().then(Promise.reject.bind(Promise));
-          })
-          .then(myRest => {
-            this.setState({
-              snackbarOpen: true,
-              success: myRest.success,
-              snackbarMsg: myRest.message
-            });
-            this.props.onAdd(myRest.data);
-          })
-          .catch(err => {
-            this.setState({
-              snackbarOpen: true,
-              success: err.success,
-              snackbarMsg: err.error
-            });
-          });
-
-      case "put":
-        return fetch(`http://localhost:6543/api/user_restaurant/${restId}`, {
-          method: "PUT",
-          headers: {
-            "Content-Type": "application/json",
-            "x-auth-token": localStorage.getItem("token")
-          },
-          body: JSON.stringify(newRest)
-        })
-          .then(response => {
-            return response.status >= 200 && response.status < 300
-              ? response.json()
-              : response.json().then(Promise.reject.bind(Promise));
-          })
-          .then(myRest => {
-            this.setState({
-              snackbarOpen: true,
-              success: myRest.success,
-              snackbarMsg: myRest.message
-            });
-            this.props.onUpdate(myRest.data);
-          })
-          .catch(err => {
-            this.setState({
-              snackbarOpen: true,
-              success: err.success,
-              snackbarMsg: err.error
-            });
-          });
-    }
-  };
-
-  handleFormChange = event => {
-    this.setState({
-      [event.target.name]: event.target.value,
-      isValid: true
-    });
-  };
-
-  handleTagsChange = event => {
-    this.setState({ tags: event.target.value });
-  };
-
-  handleCloseSnackbar = (event, reason) => {
-    if (reason === "clickaway") {
-      return;
-    }
-    this.setState({ snackbarOpen: false });
+  handleFormSubmit = e => {
+    e.preventDefault();
+    console.log(e, "submitted");
   };
 
   render() {
-    const { classes, requestType, id } = this.props;
-    const {
-      formExpanded,
-      allTags,
-      tags,
-      isValid,
-      snackbarOpen,
-      name,
-      address,
-      description,
-      phone,
-      success,
-      snackbarMsg
-    } = this.state;
+    const { classes } = this.props;
+    const { formExpanded } = this.state;
 
     return (
       <>
-        <div className={classes.header}>
-          <Typography variant="h6">
-            {requestType === "post" ? "Add new" : "Update"} restaurant:
-          </Typography>
-          <Fab
-            className={classnames(classes.fab, {
-              [classes.fabDisabled]: formExpanded
-            })}
-            onClick={this.handleExpandFormClick}
-            aria-expanded={formExpanded}
-            aria-label="Show more"
-            color="primary"
-            disabled={formExpanded}
-          >
-            {requestType === "post" ? <AddIcon /> : <Edit />}
-          </Fab>
+        <div className={classes.root}>
+          <Tooltip title="Add Restaurant" placement="left">
+            <Fab
+              className={classnames(classes.fab, {
+                [classes.fabDisabled]: formExpanded
+              })}
+              onClick={this.handleExpandFormClick}
+              aria-expanded={formExpanded}
+              aria-label="Show more"
+              color="primary"
+              disabled={formExpanded}
+            >
+              <AddIcon />
+            </Fab>
+          </Tooltip>
         </div>
         <Collapse in={formExpanded} timeout="auto" unmountOnExit>
           <Card>
+            <CardHeader
+              title={<Typography variant="h6">Add new restaurant:</Typography>}
+            />
+            <Divider />
             <CardContent>
-              <form
-                onSubmit={event => this.handleSubmit(event, requestType, id)}
-                className={classes.form}
-                noValidate
-                autoComplete="off"
-                onChange={this.handleFormChange}
-              >
-                <Grid container spacing={16}>
-                  <Grid item xs={12}>
-                    <TextField
-                      value={name}
-                      required={requestType === "post"}
-                      name="name"
-                      label="Restaurant Name"
-                      fullWidth
-                      className={classes.textField}
-                      error={!isValid}
-                      helperText={!isValid && "Required field"}
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <TextField
-                      value={address}
-                      required={requestType === "post"}
-                      name="address"
-                      label="Restaurant Address"
-                      fullWidth
-                      className={classes.textField}
-                      error={!isValid}
-                      helperText={!isValid && "Required field"}
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <TextField
-                      value={phone}
-                      name="phone"
-                      label="Restaurant Phone"
-                      fullWidth
-                      className={classes.textField}
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <TextField
-                      value={description}
-                      name="description"
-                      label="Restaurant Description"
-                      multiline
-                      rows="4"
-                      fullWidth
-                      className={classes.textField}
-                    />
-                  </Grid>
-                  <Grid container item xs={12}>
-                    <FormControl fullWidth>
-                      <InputLabel htmlFor="select-multiple-checkbox">
-                        Tags
-                      </InputLabel>
-                      <Select
-                        multiple
-                        value={tags}
-                        onChange={this.handleTagsChange}
-                        input={<Input id="select-multiple-checkbox" />}
-                        renderValue={selected => {
-                          let selectedStr = selected
-                            .map(selectedItem => selectedItem.name)
-                            .join(", ");
-
-                          return selectedStr;
-                        }}
-                        MenuProps={MenuProps}
-                      >
-                        {allTags.map(tag => (
-                          <MenuItem key={tag.id} value={tag}>
-                            <Checkbox checked={tags.indexOf(tag) > -1} />
-                            <ListItemText primary={tag.name} />
-                          </MenuItem>
-                        ))}
-                      </Select>
-                    </FormControl>
-                  </Grid>
-                  <Grid
-                    className={classes.btns}
-                    container
-                    item
-                    xs={12}
-                    justify="space-between"
-                  >
-                    <Grid item xs={3}>
-                      <Button
-                        onClick={this.handleCloseFormClick}
-                        variant="contained"
-                        color="secondary"
-                        fullWidth
-                      >
-                        Cancel
-                      </Button>
-                    </Grid>
-                    <Grid item xs={3}>
-                      <Button
-                        type="submit"
-                        variant="contained"
-                        color="primary"
-                        fullWidth
-                      >
-                        {requestType === "post" ? "Add" : "Update"}
-                      </Button>
-                    </Grid>
-                  </Grid>
-                </Grid>
-              </form>
+              <AddRestaurantForm />
             </CardContent>
           </Card>
         </Collapse>
-        <Snackbar
-          anchorOrigin={{
-            vertical: "bottom",
-            horizontal: "right"
-          }}
-          open={snackbarOpen}
-          autoHideDuration={3000}
-          onClose={this.handleCloseSnackbar}
-          ContentProps={{
-            "aria-describedby": "message-id",
-            className: success ? classes.snackbarSuccess : classes.snackbarError
-          }}
-          message={<span id="message-id">{snackbarMsg}</span>}
-          action={[
-            <IconButton
-              key="close"
-              aria-label="Close"
-              color="inherit"
-              className={classes.close}
-              onClick={this.handleCloseSnackbar}
-            >
-              <CloseIcon />
-            </IconButton>
-          ]}
-        />
       </>
     );
   }
