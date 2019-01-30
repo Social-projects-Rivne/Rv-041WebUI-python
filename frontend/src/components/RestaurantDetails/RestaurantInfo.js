5212a7725ceaecc4260f70679437bccfb5ce6bbb
class <mailmax67@gmail.com>
2019-01-30 13:13:49 +0200

    changed loyaut of details

--- frontend/src/components/RestaurantDetails/RestaurantInfo.js
+++ frontend/src/components/RestaurantDetails/RestaurantInfo.js
@@ -6,87 +6,96 @@ import {
   Typography,
   CardContent,
   Button,
-  Grid
+  Grid,
+  CardMedia
 } from "@material-ui/core/";
-import {Link} from "react-router-dom";
-import {amber, green, red} from "@material-ui/core/colors/index";
+import { Link } from "react-router-dom";
+import { amber, green, red } from "@material-ui/core/colors/index";
 
-const styles = {
+const styles = theme => ({
   root: {},
   active: {
-        background: green[500]
-    },
+    background: green[500]
+  },
   notApproved: {
-        background: amber[700]
-    },
-  btn:{
-      background: red[700],
-      color: "white"
+    background: amber[700]
+  },
+  btn: {
+    background: red[700],
+    color: "white"
+  },
+  media: {
+    flex: 1,
+    minWidth: theme.spacing.unit * 30,
+    minHeight: theme.spacing.unit * 30
   }
-};
+});
 const deleteStatus = 2;
-function handleRestaurantDelete()  {
+function handleRestaurantDelete() {
+  const headers = new Headers({
+    "Content-Type": "application/json",
+    "X-Auth-Token": localStorage.getItem("token")
+  });
 
-    const headers = new Headers({
-        'Content-Type': 'application/json',
-        'X-Auth-Token': localStorage.getItem("token")
-    });
+  const fetchInit = {
+    method: "PUT",
+    headers: headers,
+    body: JSON.stringify({
+      id: this.props.restInfo.id,
+      status: this.props.restInfo.status
+    })
+  };
 
-    const fetchInit = {
-        method: "PUT",
-        headers: headers,
-        body: JSON.stringify({id: this.props.restInfo.id, status: this.props.restInfo.status}),
-    };
-
-    fetch('http://localhost:6543/api/delete_restaurant', fetchInit)
-        .then(response => (!(response.status >= 200 && response.status < 300)
-            ? Promise.reject(response.status)
-            : response.json()));
-
+  fetch("http://localhost:6543/api/delete_restaurant", fetchInit).then(
+    response =>
+      !(response.status >= 200 && response.status < 300)
+        ? Promise.reject(response.status)
+        : response.json()
+  );
 }
 
 class RestaurantInfo extends React.Component {
   render() {
-    const {restInfo, auth, ableUpdate } = this.props;
+    const { classes, restInfo, auth, ableUpdate } = this.props;
     return (
       <Card>
         <CardContent>
-          <div
-            style={{
-              width: "300px",
-              height: "300px",
-              backgroundColor: "#dadada",
-              float: "left",
-              marginRight: "16px"
-            }}
-            className="gallery"
-          />
-          <div className="content">
-            <Typography gutterBottom variant="h4">
-              {restInfo.name}
-            </Typography>
+          <Grid container spacing={16}>
+            <Grid item xs={3}>
+              <CardMedia
+                className={classes.media}
+                image="https://media-cdn.tripadvisor.com/media/photo-f/04/43/20/9c/whisky-corner.jpg"
+                title={restInfo.name}
+              />
+            </Grid>
+            <Grid item xs={9}>
+              <Typography gutterBottom variant="h4">
+                {restInfo.name}
+              </Typography>
 
-            <Typography gutterBottom variant="h6" component="p">
-              Address: {restInfo.address_id} <br />
-              Phone: {restInfo.phone}
-            </Typography>
-            <Divider style={{ marginBottom: "2em" }} />
-            <Typography variant="body1" gutterBottom component="p">
-              {restInfo.description}
-            </Typography>
-              {auth && ableUpdate && (restInfo.status!==deleteStatus) &&(<Grid container justify="flex-end">
+              <Typography gutterBottom variant="h6" component="p">
+                Address: {restInfo.address_id} <br />
+                Phone: {restInfo.phone}
+              </Typography>
+              <Divider style={{ marginBottom: "2em" }} />
+              <Typography variant="body1" gutterBottom component="p">
+                {restInfo.description}
+              </Typography>
+              {auth && ableUpdate && restInfo.status !== deleteStatus && (
+                <Grid container justify="flex-end">
                   <Button
-                      variant="contained"
-                      className={this.props.classes.btn}
-                      component={Link}
-                      to={"/profile/my_restaurants/"}
-                      onClick={handleRestaurantDelete.bind(this)}
+                    variant="contained"
+                    className={this.props.classes.btn}
+                    component={Link}
+                    to={"/profile/my_restaurants/"}
+                    onClick={handleRestaurantDelete.bind(this)}
                   >
-                      delete
+                    delete
                   </Button>
-              </Grid>)}
-
-          </div>
+                </Grid>
+              )}
+            </Grid>
+          </Grid>
         </CardContent>
       </Card>
     );
