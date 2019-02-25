829ef435bf358afd9e2388cae048a50094658fa0
Ivan A <riquadro2012@gmail.com>
2019-02-25 13:40:53 +0200

    Edit "elif" statement

--- easyrest/views/auth_controler.py
+++ easyrest/views/auth_controler.py
@@ -33,7 +33,7 @@ def login_post(request):
 
     if user is None or not pbkdf2_sha256.verify(password, user.password):
         raise HTTPForbidden("Email or password is invalid")
-    elif user.is_active is False:
+    elif not user.is_active:
         raise HTTPForbidden("Sorry, you have been blocked")
 
     token = remember(request, user)
