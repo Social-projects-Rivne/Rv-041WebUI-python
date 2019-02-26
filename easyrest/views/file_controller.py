99a7f9e70277fe249c895462b8b06f5d0f9df753
Max <m.sukhovetskyi@gmail.com>
2019-02-26 11:51:45 +0200

    Backend of file upload

--- easyrest/views/file_controller.py
+++ easyrest/views/file_controller.py
@@ -1,6 +1,13 @@
+import os
+import uuid
+import shutil
+
 from pyramid.view import view_config
+from pyramid.httpexceptions import HTTPBadRequest
 
 from ..auth import restrict_access
+
+img_index = 0
 
 
 @view_config(
@@ -8,12 +15,37 @@ from ..auth import restrict_access
     renderer='json',
     request_method='POST'
 )
-def upload_skadl_(request):
-    print request
-    # try:
-    #     filename = request.POST["img"]
-    # except KeyError:
-    #     print("heh")
+def upload(request):
+    global img_index
+    filename = request.POST['img'].filename
+    input_file = request.POST['img'].file
 
-    # print filename
-    return "dasdasd"
+    if not filename.lower().endswith(('.png', '.jpg', '.jpeg')):
+        raise HTTPBadRequest("Wrong file type")
+
+    file_type = filename.split(".")[-1]
+
+    print(file_type)
+
+    new_filename = uuid.uuid4()
+
+    file_path = os.path.join(
+        '%s/easyrest/static/images' % os.getcwd(), '%s%d.%s' % (new_filename,
+                                                                img_index,
+                                                                file_type))
+
+    temp_file_path = file_path + '~'
+
+    input_file.seek(0)
+    with open(temp_file_path, 'wb') as output_file:
+        shutil.copyfileobj(input_file, output_file)
+
+    os.rename(temp_file_path, file_path)
+
+    url_root = "http://localhost:6543/api/static/%s%s.%s" % (new_filename,
+                                                             img_index,
+                                                             file_type)
+
+    img_index += 1
+
+    return url_root
